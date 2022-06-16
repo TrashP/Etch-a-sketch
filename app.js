9bf84e2e8117e075dbad89727fe5f54bc52e6656
Arnab <arnabdnri@gmail.com>
2022-06-16 08:43:40 +0800

    Add UI for entering number of cells

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('.num-button');
 
 const rows = [];
 const columns = [];
+input = 0;
 
-for (i = 0; i < 16; i++) {
-    rows[i] = document.createElement('div');
-    container.appendChild(rows[i]);
-    rows[i].style.display = 'flex';
-
-    rows[i].style.margin = 0;
-    rows[i].style.padding = 0;
-    for (j = 0; j < 16; j++) {
-        columns[j] = document.createElement('div');
-        rows[i].appendChild(columns[j]);
-        columns[j].classList.add('table-cell');
+button.addEventListener('click', () => {
+    input = parseInt(prompt('Enter the number of cells per side: (max 100)', 16));
+    if (input > 100) {
+        input = 100;
     }
-}
-
-container.addEventListener('mouseover', e => {
-    if (e.target.matches('.table-cell')) {
-        e.target.classList.add('hover-mouse');
+    
+    for (i = 0; i < input; i++) {
+        rows[i] = document.createElement('div');
+        container.appendChild(rows[i]);
+        rows[i].classList.add('table-row');
+    
+        for (j = 0; j < input; j++) {
+            columns[j] = document.createElement('div');
+            rows[i].appendChild(columns[j]);
+            columns[j].classList.add('table-cell');
+        }
     }
+    
+    container.addEventListener('mouseover', e => {
+        if (e.target.matches('.table-cell')) {
+            e.target.classList.add('hover-mouse');
+        }
+    })
 })
+
